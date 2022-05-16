e3ce766546a6beccee254f64b87e7b8d1f9e095e
HeshamM-088 <heshamkhalil1988@gmail.com>
2022-05-17 01:22:32 +0200

    original

---
+++ main.js
@@ -0,0 +1,64 @@
+// var myName = document.getElementById("test") ;
+
+// myName.innerHTML = "adham";
+
+// window.alert("hello mr adham");
+
+// var myName = "hesham";
+
+// var mySalary = 1500 
+
+// var ourNames = ["hesham","taher","mohamed","ahmed"]
+
+// var xCheck = true
+
+// var x = null 
+
+// var y;
+
+// var myObj = {
+//     names: "hesham",
+//     age: 20
+// }
+
+// myObj.age = 15;
+
+
+
+
+
+// var x , y ;
+ 
+//   x = 5 ;
+
+//   y = 10 ; 
+
+//   y = y + x ;
+
+//   y = "hesham";
+
+// console.log(y);
+
+// var x , total , count , y;
+
+//  x = [10 , 20 , 30 , 40];
+//  console.log(x);
+
+
+//  total = x[0] + x[1] + x[2] + x[3];
+//  console.log(total);
+
+//  count = total / 4 ;
+//  console.log(count);
+
+// document.getElementById("test").innerHTML = count;
+
+
+var y , x ;
+
+y = 5 ;
+x = 10 ;
+
+document.getElementById("test").innerHTML = x < y;
+
+
